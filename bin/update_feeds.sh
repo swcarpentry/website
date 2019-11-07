6a83e2f1548ba6e75026f3562b8e0e88ad6a2803
François Michonneau <francois.michonneau@gmail.com>
2019-11-07 11:31:22 +0100

    build dashboard data

--- bin/update_feeds.sh
+++ bin/update_feeds.sh
@@ -4,6 +4,7 @@ REPO_ORG=swcarpentry
 REPO_NAME=website
 GITHUB_PAT_USER=fmichonneau # user who generated the GITHUB PAT used here
 
+## Get workshop data from data feed
 cd .. &&
     mkdir feed-data &&
     cd feed-data &&
@@ -12,10 +13,19 @@ cd .. &&
 
 cd ../"$REPO_NAME"  || exit
 
+## Generate dashboard data
+if [ ! -s ${HOME}/.git-token ]
+then
+    echo "${GITHUB_PAT}" > ${HOME}/.git-token;
+fi
+make dashboard
+
+## Push changes to repository
 git remote add deploy https://"$GITHUB_PAT_USER":"$GITHUB_PAT"@github.com/"$REPO_ORG"/"$REPO_NAME".git
 
 git checkout gh-pages
 git add _data/*.json
+git add _data/dashboard.yml
 git commit -m "[ci skip] update workshop data"
 git push deploy gh-pages
 
